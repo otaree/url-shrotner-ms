41691678abc08af574d7a2c22c0ce60e67f3458f
otaree <deep.yen.ur.hash@gmail.com>
2018-02-22 11:18:54 +0530

    Add fcc favicon

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +19,7 @@ const {
 } = require('./utils/urlutils');
 
 app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, '/..','public', 'img', 'favicon-16x16.png')));
 
 app.get('/', (req, res) => {
     res.sendFile('/index.html');
